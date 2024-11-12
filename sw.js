8f1885b2596b5aad3a97571dcad4e3caf2457b1e
effect8010 <effect8010@gmail.com>
2024-11-12 18:35:01 +0900

    add pwa files

--- sw.js
+++ sw.js
@@ -1,16 +1,21 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('level-meter-store').then((cache) => cache.addAll([
-      '/',
-      '/index.html',
-      '/icon.png',
-      '/manifest.json'
-    ])),
+const CACHE_NAME = 'level-meter-v1';
+const urlsToCache = [
+  '.',
+  'index.html',
+  'icon.png',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
